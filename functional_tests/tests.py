2bd6af72a40331eed8f62100cb3f707cfb73d929
Daniel Chia <daniel_1118@hotmail.com>
2015-11-14 16:04:04 -0500

    New URL, view and template to display lists

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -41,7 +41,11 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys('Kendama tricks to learn')
 
         #on enter, the page updates -> '1. Kendama tricks to learn'
+        #take to a new url
         inputbox.send_keys(Keys.ENTER)
+        #checks that there has been a redirect
+        edith_list_url = self.browser.current_url
+        self.assertRegexpMatches(edith_list_url, '/lists/.+')
         self.check_for_row_in_list_table('1: Kendama tricks to learn')
 
         #there's still a textbox for more entries, he types, 'Around Japan'
@@ -51,6 +55,37 @@ class NewVisitorTest(LiveServerTestCase):
 
         self.check_for_row_in_list_table('1: Kendama tricks to learn')
         self.check_for_row_in_list_table('2: Around Japan')
+
+        #************************************************************************
+        #new user coming along, Francis
+        ## We use a new browser session to make sure that no information
+        ## of Edith's is coming through from cookies etc #1
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        # Francis visits the home page.  There is no sign of Edith's
+        # list
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Kendama tricks', page_text)
+        self.assertNotIn('to learn', page_text)
+
+        # Francis starts a new list by entering a new item. He
+        # is less interesting than Edith...
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+
+        # Francis gets his own unique URL
+        francis_list_url = self.browser.current_url
+        self.assertRegexpMatches(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        # Again, there is no trace of Edith's list
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
+
 
         #reminder message
         self.fail('Finish the test!') 
