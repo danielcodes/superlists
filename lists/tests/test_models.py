7782c821ba17336a8e2caa36814c5ea8c5d3754d
Daniel Chia <daniel_1118@hotmail.com>
2016-01-08 17:42:10 -0500

    Split out unit tests into two files

---
+++ lists/tests/test_models.py
@@ -0,0 +1,40 @@
+from django.test import TestCase
+from lists.models import Item, List
+
+class ListAndItemModelsTest(TestCase):
+
+    #throw data in, and check data out
+    def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        #the item has a list??
+        #what the heck does .list do???
+        #item has a list
+        first_item.list = list_
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        #second item's list is also list_
+        second_item.list = list_
+        second_item.save()
+
+        #first list
+        #get first list
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        #testing constangs here aren't we
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(first_saved_item.list, list_)
+        self.assertEqual(second_saved_item.text, 'Item the second')
+        self.assertEqual(second_saved_item.list, list_)
+
--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -28,44 +28,6 @@ class HomePageTest(TestCase):
         #content returns bytes, decode to python unicode string
         self.assertEqual(response.content.decode(), expected_html)
 
-#this tests the models
-class ListAndItemModelsTest(TestCase):
-
-    #throw data in, and check data out
-    def test_saving_and_retrieving_items(self):
-        list_ = List()
-        list_.save()
-
-        first_item = Item()
-        first_item.text = 'The first (ever) list item'
-        #the item has a list??
-        #what the heck does .list do???
-        #item has a list
-        first_item.list = list_
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = 'Item the second'
-        #second item's list is also list_
-        second_item.list = list_
-        second_item.save()
-
-        #first list
-        #get first list
-        saved_list = List.objects.first()
-        self.assertEqual(saved_list, list_)
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        #testing constangs here aren't we
-        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
-        self.assertEqual(first_saved_item.list, list_)
-        self.assertEqual(second_saved_item.text, 'Item the second')
-        self.assertEqual(second_saved_item.list, list_)
-
 
 #create a list and pass it to a url path
 #check that it is using the respective template
@@ -168,5 +130,3 @@ class NewItemTest(TestCase):
         response = self.client.get('/lists/%d/' % (correct_list.id,))
         self.assertEqual(response.context['list'], correct_list)
     
-
-
