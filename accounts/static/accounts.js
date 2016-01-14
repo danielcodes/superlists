afa15d6fb2c2706a4cf000b4586655d356133b6b
Daniel Chia <daniel_1118@hotmail.com>
2016-01-13 19:44:28 -0500

    Wrapped up chapter15, FT failing, used sinon to mock login

---
+++ accounts/static/accounts.js
@@ -0,0 +1,35 @@
+//small chain here, superlists gets called from the test,
+//it then calls account and it's initialize function
+//whatever object is passed, we take the id key and call request
+//request sets the requestcalled var to true
+
+var initialize = function (navigator, user, token, urls) {
+	$('#id_login').on('click', function() {
+		navigator.id.request();
+	});
+
+	//3 step tdd cycle, create function, do an ajax call, 
+	//get the login url, and pass assertion
+	navigator.id.watch({
+		loggedInUser: user,
+		//what the heck is this browserid assertion
+		onlogin: function(assertion) {
+			$.post(
+				urls.login,
+				{assertion: assertion, csrfmiddlewaretoken: token}
+			)
+				.done(function () { window.location.reload(); })
+                .fail(function () { navigator.id.logout(); });
+		},
+		onlogout: function() {}	
+	});
+};
+
+window.Superlists = {
+	Accounts: {
+		initialize: initialize 
+	}
+};
+
+
+
